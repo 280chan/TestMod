79f0d9850d84efd95efc2d24ff6abf839612ef83
incubator4th <wcz@uenit.cn>
2020-07-08 17:57:31 +0800

    daily

--- src/utils/MahjongWall.java
+++ src/utils/MahjongWall.java
@@ -1,17 +1,53 @@
 package utils;
 
-public class MahjongWall implements SaveDataTransfer {
-    /** 麻将 牌山默认种类 */
-    public static final String[] YAMA_NAME = {"w0", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9", "p0", "p1",
-            "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9",
-            "z1", "z2", "z3", "z4", "z5", "z6", "z7"};
-    /** 麻将 牌山默认种类对应的默认数量 */
-    public static final int[] YAMA_DEFAULT = {1, 4, 4, 4, 4, 3, 4, 4, 4, 4, 1, 4, 4, 4, 4, 3, 4, 4, 4, 4, 1, 4, 4, 4,
-            4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
-    public MahjongWall() {
-    }
-    public MahjongWall(String value){
+import cards.mahjong.AbstractMahjongCard;
 
+import java.util.*;
+
+public class MahjongWall implements SaveDataTransfer {
+    /**
+     * 麻将 牌山默认种类
+     */
+    public static final Map WallDefault = new HashMap<String, Integer>() {
+        {
+            put("1", 1);
+        }
+    };
+    /**
+     * 麻将默认牌山数量
+     */
+    public static final int DEFAULT_WALL_LENGTH = 136;
+
+    private TreeMap<String, Integer> currentWall;
+    private int currentWallLength;
+    /** 麻将 当前遗物 牌河历史记录的卡牌ID */
+    private final ArrayList<Integer> historyDiscardList = new ArrayList<>();
+
+    private final Random randomNumberGenerate;
+
+    public MahjongWall(Random randomNumberGenerate) {
+        this.randomNumberGenerate = randomNumberGenerate;
+        initialize();
+    }
+
+    private void initialize() {
+        currentWall = new TreeMap<>();
+        WallDefault.forEach((key, value) -> {
+            currentWall.put((String) key, (Integer) value);
+        });
+        currentWallLength = DEFAULT_WALL_LENGTH;
+    }
+
+
+    public AbstractMahjongCard Draw() {
+        return null;
+    }
+
+
+    private String generateRandomCard() {
+        int index = this.randomIndex(this.currentWallLength, this.historyDiscardSeed());
+        currentWall.higherEntry().getValue()
+                // todo  https://leetcode-cn.com/problems/random-pick-with-weight/solution/lun-pan-du-suan-fa-by-joyboy/
     }
 
     @Override
@@ -22,5 +58,20 @@ public class MahjongWall implements SaveDataTransfer {
     @Override
     public void UnMarshal() {
 
+    }
+
+    private int randomIndex(int range, long seedFix) {
+        for (long i = 0; i < seedFix; i++)
+            randomNumberGenerate.nextBoolean();
+        return randomNumberGenerate.nextInt(range);
+    }
+
+    private long historyDiscardSeed() {
+        long seed = 0L;
+        for (Integer i : this.historyDiscardList) {
+            seed += i.longValue() + 1L;
+            seed *= i.longValue() + 1L;
+        }
+        return seed;
     }
 }
