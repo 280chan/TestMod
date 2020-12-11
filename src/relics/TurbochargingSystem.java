fdc0b585420aa1fedb953dd7bcd9be2363a66463
280chan <42588049+280chan@users.noreply.github.com>
2020-12-11 06:34:50 -0700

    Add a new relic

---
+++ src/relics/TurbochargingSystem.java
@@ -0,0 +1,33 @@
+package relics;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer.PlayerClass;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+public class TurbochargingSystem extends AbstractTestRelic {
+	public static final String ID = "TurbochargingSystem";
+	
+	public TurbochargingSystem() {
+		super(ID, RelicTier.COMMON, LandingSound.HEAVY);
+	}
+	
+	public String getUpdatedDescription() {
+		return DESCRIPTIONS[0];
+	}
+
+	public void updateDescription(PlayerClass c) {
+		this.tips.clear();
+	    this.tips.add(new PowerTip(this.name, this.getUpdatedDescription()));
+	    initializeTips();
+	}
+	
+	public float atDamageModify(float damage, AbstractCard c) {
+		if (c.freeToPlayOnce) {
+			return damage;
+		} else if (c.cost == -1) {
+			return damage + Math.max(c.energyOnUse, EnergyPanel.totalCount);
+		}
+		return damage + c.costForTurn;
+	}
+}
