67220ae378d081a1a7d4a8fe32dd2e5d7ea79d03
280chan <42588049+280chan@users.noreply.github.com>
2022-02-28 06:48:08 -0700

    improve

--- src/events/BoxForYourself.java
+++ src/events/BoxForYourself.java
@@ -4,61 +4,37 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
-
 import mymod.TestMod;
 import screens.BoxForYourselfSelectScreen;
 
 public class BoxForYourself extends AbstractTestEvent {
-	private CUR_SCREEN screen = CUR_SCREEN.INTRO;
-
 	private AbstractRelic obtainRelic = null;
 	private static AbstractRelic saveRelic = null;
 	private boolean relicSelect = false;
 	private static int indexSelected = -1;
 	private static int floorNum = -2;
-	
-	private static enum CUR_SCREEN {
-		INTRO, CHOOSE, COMPLETE;
 
-		private CUR_SCREEN() {
-		}
-	}
-
-	public BoxForYourself() {
-		super();
-		this.imageEventText.setDialogOption(option()[0]);
+	@Override
+	protected void intro() {
 		initializeObtainRelic();
+		this.imageEventText.updateBodyText(desc()[1]);
+		this.imageEventText.updateDialogOption(0, option()[3]);
+		this.imageEventText.setDialogOption(option()[1] + this.obtainRelic.name + option()[2], this.obtainRelic);
 	}
 
-	protected void buttonEffect(int buttonPressed) {
-		switch (this.screen) {
-		case INTRO:
-			this.imageEventText.updateBodyText(desc()[1]);
-			this.screen = CUR_SCREEN.CHOOSE;
-			this.imageEventText.updateDialogOption(0, option()[1] + this.obtainRelic.name + option()[2]);
-			this.imageEventText.setDialogOption(option()[3]);
+	@Override
+	protected void choose(int choice) {
+		switch (choice) {
+		case 0:
+			getRelic();
+			saveRelic();
 			break;
-		case CHOOSE:
-			this.screen = CUR_SCREEN.COMPLETE;
-			AbstractDungeon.getCurrRoom().phase = RoomPhase.COMPLETE;
-			switch (buttonPressed) {
-			case 0:
-				getRelic();
-				saveRelic();
-				break;
-			default:
-				logMetricIgnored(this.title);
-			}
-			this.imageEventText.updateBodyText(desc()[3]);
-			this.imageEventText.updateDialogOption(0, option()[4]);
-			this.imageEventText.clearRemainingOptions();
-			this.screen = CUR_SCREEN.COMPLETE;
-			AbstractDungeon.getCurrRoom().phase = RoomPhase.COMPLETE;
-			break;
-		case COMPLETE:
-			openMap();
+		default:
+			logMetricIgnored(this.title);
 		}
+		this.imageEventText.updateBodyText(desc()[3]);
+		this.imageEventText.updateDialogOption(0, option()[4]);
+		this.imageEventText.clearRemainingOptions();
 	}
 
 	private void initializeObtainRelic() {
