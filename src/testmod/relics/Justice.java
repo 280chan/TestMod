7e2a851ff55b6fd961ae3e2312d431985115c959
280chan <280262810@qq.com>
2022-05-02 01:02:09 -0700

    rewrite code

--- src/testmod/relics/Justice.java
+++ src/testmod/relics/Justice.java
@@ -1,23 +1,22 @@
 package testmod.relics;
 
-import testmod.powers.JusticePower;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnReceivePowerRelic;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.AbstractPower.PowerType;
 
-public class Justice extends AbstractTestRelic {
+public class Justice extends AbstractTestRelic implements OnReceivePowerRelic {
 	
 	public Justice() {
 		super(RelicTier.UNCOMMON, LandingSound.SOLID);
 	}
+
+	@Override
+	public boolean onReceivePower(AbstractPower p, AbstractCreature source) {
+		if (p.type == PowerType.DEBUFF)
+			relicStream(Justice.class).peek(r -> r.show()).forEach(r -> att(apply(p(), new StrengthPower(p(), 1))));
+		return true;
+	}
 	
-	public void atPreBattle() {
-		if (!isActive)
-			return;
-		p().powers.add(new JusticePower(p()));
-    }
-	
-	public void atTurnStart() {
-		if (!isActive)
-			return;
-		if (!JusticePower.hasThis(p()))
-			this.addTmpActionToTop(() -> p().powers.add(new JusticePower(p())));
-    }
 }
