9008ed688160060551cf3515ce6b589b9ca62f0c
280chan <280262810@qq.com>
2022-05-09 11:52:34 -0700

    nothing

--- src/testmod/commands/Test.java
+++ src/testmod/commands/Test.java
@@ -1,9 +1,14 @@
 package testmod.commands;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
 
+import testmod.cards.AbstractTestCard;
+import testmod.cards.colorless.SunMoon;
+import testmod.cards.colorless.WeaknessCounterattack;
 import testmod.mymod.TestMod;
 
 @SuppressWarnings("unused")
@@ -14,7 +19,10 @@ public class Test extends TestCommand {
 			cmdHelp();
 			return;
 		}
-		
+		p().hand.group.forEach(c -> {
+			c.retain = true;
+			c.isEthereal = true;
+		});
 		/*p().masterDeck.group.clear();
 		TestMod.CARDS.stream().map(c -> c.makeCopy()).forEach(p().masterDeck.group::add);*/
 		
@@ -23,4 +31,5 @@ public class Test extends TestCommand {
 	private static void cmdHelp() {
 		tooManyTokensError();
 	}
+	
 }
