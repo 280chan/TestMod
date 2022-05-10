f18ac1a974f10ed282dddf032ab24551f4187882
280chan <280262810@qq.com>
2022-05-10 11:26:42 -0700

    nerf and improve

--- src/testmod/relics/Extravagant.java
+++ src/testmod/relics/Extravagant.java
@@ -10,13 +10,14 @@ import basemod.BaseMod;
 
 public class Extravagant extends AbstractTestRelic implements ClickableRelic {
 	private static int delta = 0;
+	private boolean playerTurn = false;
+	
+	public static void updateCounter() {
+		MISC.relicStream(Extravagant.class).forEach(r -> r.counter = BaseMod.MAX_HAND_SIZE);
+	}
 	
 	public Extravagant() {
 		super(RelicTier.BOSS, LandingSound.MAGICAL);
-	}
-	
-	public String getUpdatedDescription() {
-		return DESCRIPTIONS[0];
 	}
 	
 	private void updateAllPulse() {
@@ -24,11 +25,10 @@ public class Extravagant extends AbstractTestRelic implements ClickableRelic {
 	}
 	
 	private void up(boolean stop) {
-		if (!stop && BaseMod.MAX_HAND_SIZE > 0)
+		if ((this.playerTurn = !stop) && BaseMod.MAX_HAND_SIZE > 0)
 			this.beginLongPulse();
 		else
 			this.stopPulse();
-		this.counter = stop ? -1 : -2;
 	}
 	
 	public void onEquip() {
@@ -45,6 +45,7 @@ public class Extravagant extends AbstractTestRelic implements ClickableRelic {
 	public void atPreBattle() {
 		if (this.isActive)
 			delta = 0;
+		this.counter = BaseMod.MAX_HAND_SIZE;
     }
 	
 	public void atTurnStart() {
@@ -60,6 +61,7 @@ public class Extravagant extends AbstractTestRelic implements ClickableRelic {
 		if (delta > 0) {
 			BaseMod.MAX_HAND_SIZE += delta;
 			delta = 0;
+			HyperplasticTissue.updateCounter();
 		}
     }
 	
@@ -72,25 +74,25 @@ public class Extravagant extends AbstractTestRelic implements ClickableRelic {
 	private Consumer<AbstractCard> play(int time) {
 		return c -> {
 			AbstractDungeon.getCurrRoom().souls.remove(c);
-			this.addTmpActionToTop(() -> {
-				for (int i = 1; i < time; i++)
-					play(c.makeSameInstanceOf(), true);
-				play(c, false);
-			});
+			for (int i = 1; i < time; i++)
+				play(c.makeSameInstanceOf(), true);
+			play(c, false);
 		};
 	}
 
 	@Override
 	public void onRightClick() {
-		if (this.inCombat() && this.counter == -2 && BaseMod.MAX_HAND_SIZE > 0 && !p().hand.group.isEmpty()) {
+		if (this.inCombat() && this.playerTurn && BaseMod.MAX_HAND_SIZE > 0 && !p().hand.group.isEmpty()) {
 			this.show();
 			int tmp = Math.max(1, BaseMod.MAX_HAND_SIZE / 2);
 			delta += tmp;
 			BaseMod.MAX_HAND_SIZE -= tmp;
+			HyperplasticTissue.updateCounter();
 			this.updateAllPulse();
-			tmp = p().hand.group.size();
-			reverse(p().hand.group).forEach(play(tmp));
-			p().hand.group.clear();
+			if ((tmp = Math.min(p().energy.energyMaster, p().hand.group.size())) > 0) {
+				reverse(p().hand.group).forEach(play(tmp));
+				p().hand.group.clear();
+			}
 		}
 	}
 
