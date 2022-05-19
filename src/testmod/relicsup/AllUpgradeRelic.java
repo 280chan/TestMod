642ef5953b91dd672b5554db6a44a0ef1edf963a
280chan <280262810@qq.com>
2022-05-19 10:42:59 -0700

    improve

--- src/testmod/relicsup/AllUpgradeRelic.java
+++ src/testmod/relicsup/AllUpgradeRelic.java
@@ -1,5 +1,8 @@
 package testmod.relicsup;
 
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
 import relicupgradelib.arch.*;
 import testmod.relics.*;
 
@@ -13,21 +16,36 @@ public class AllUpgradeRelic {
 			p.addBranch(new UpgradeBranch(r1, null, red, green, blue, gold));
 			ProxyManager.register(p);
 		}
-		private static void set(AbstractTestRelic r, AbstractTestRelic r1, boolean red, boolean green, boolean blue) {
-			set(r, r1, red, green, blue, 0);
-		}
-		private static void set(AbstractTestRelic r, AbstractTestRelic r1, int gold) {
-			set(r, r1, false, false, false, gold);
-		}
 	}
 	
-	public static void add() {
-		Register.set(new TestBox(), new TestBoxUp(), 99);
-		Register.set(new AbnormalityKiller(), new AbnormalityKillerUp(), 150);
-		Register.set(new Acrobat(), new AcrobatUp(), 50);
-		Register.set(new Alchemist(), new AlchemistUp(), true, false, false);
-		
+	public static void add(AbstractTestRelic r) {
+		String original = r.getClass().getSimpleName();
+		UIStrings tmp = CardCrawlGame.languagePack.getUIString(UIID(original));
+		int key = Integer.parseInt(tmp.TEXT[0]);
+		int gold = Integer.parseInt(tmp.TEXT[1]);
+		// blue * 4 + green * 2 + red
+		boolean red = key % 2 == 1;
+		boolean green = (key % 4) / 2 == 1;
+		boolean blue = key / 4 == 1;
+		AbstractUpgradedRelic up = upgrade(original);
+		if (up != null)
+			Register.set(r, up, red, green, blue, gold);
 	}
 	
+	private static String UIID(String original) {
+		return "testmod-upgrade-" + original;
+	}
+	
+	private static String fullName(String original) {
+		return "testmod.relicsup." + original + "Up";
+	}
+	
+	public static AbstractUpgradedRelic upgrade(String original) {
+		try {
+			return (AbstractUpgradedRelic) Class.forName(fullName(original)).newInstance();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			return null;
+		}
+	}
 	
 }
