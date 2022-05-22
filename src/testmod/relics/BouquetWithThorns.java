bdb68ef807f7fbca47c0460a5620292789ce86bf
280chan <280262810@qq.com>
2022-05-22 11:22:17 -0700

    imporve

--- src/testmod/relics/BouquetWithThorns.java
+++ src/testmod/relics/BouquetWithThorns.java
@@ -1,11 +1,8 @@
 package testmod.relics;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.ThornsPower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
 
 public class BouquetWithThorns extends AbstractTestRelic {
 	
@@ -14,19 +11,21 @@ public class BouquetWithThorns extends AbstractTestRelic {
 	}
 	
 	private void increaseThorns(int amount) {
-		this.flash();
-		this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ThornsPower(AbstractDungeon.player, amount), amount));
+		if (amount > 0) {
+			this.flash();
+			this.addToTop(apply(p(), new ThornsPower(p(), amount)));
+		}
 	}
 	
 	public void atPreBattle() {
 		increaseThorns(1);
     }
 	
-	public int onAttacked(final DamageInfo info, final int damageAmount) {
-		if (AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == RoomPhase.COMBAT && info.type != DamageType.THORNS) {
-			increaseThorns(info.type == DamageType.NORMAL ? (damageAmount > 0 ? 2 : 1) : damageAmount);
+	public int onAttacked(final DamageInfo info, final int damage) {
+		if (this.inCombat() && info.type != DamageType.THORNS) {
+			increaseThorns(info.type == DamageType.NORMAL ? (damage > 0 ? 2 : 1) : damage);
 		}
-		return damageAmount;
+		return damage;
     }
 	
 }
