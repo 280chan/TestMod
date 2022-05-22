a2a9d67e07122451b6574ea84d58f8b5cb76dc3a
280chan <280262810@qq.com>
2022-05-22 11:22:06 -0700

    improve

--- src/testmod/powers/PortableTranscriptPower.java
+++ src/testmod/powers/PortableTranscriptPower.java
@@ -1,10 +1,15 @@
 package testmod.powers;
 
+import java.util.HashMap;
+
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public class PortableTranscriptPower extends AbstractTestPower {
+	
+	private static final HashMap<AbstractCard, Boolean> PRE = new HashMap<AbstractCard, Boolean>();
 	
 	public PortableTranscriptPower(int amount) {
 		this.owner = p();
@@ -17,14 +22,40 @@ public class PortableTranscriptPower extends AbstractTestPower {
 		 this.description = desc(0) + this.amount + desc(1);
 	}
 	
+	private int fakeEnergy() {
+		int base = p().energy.energy;
+		if (p().hasRelic("Ice Cream") || p().hasPower("Conserve"))
+			base += EnergyPanel.totalCount;
+		return base;
+	}
+	
 	private void act(AbstractCard c) {
+		if (c.cost == -1) {
+			c.energyOnUse = fakeEnergy();
+			PRE.compute(c, (a, b) -> c.freeToPlayOnce);
+			c.freeToPlayOnce = true;
+		}
 		for (int i = 0; i < this.amount; i++)
-			c.use(p(), AbstractDungeon.getCurrRoom().monsters.getRandomMonster(true));
+			c.use(p(), AbstractDungeon.getRandomMonster());
 		c.isEthereal = true;
+		if (c.cost == -1) {
+			this.addTmpActionToBot(() -> {
+				c.freeToPlayOnce = PRE.getOrDefault(c, false);
+				PRE.remove(c);
+			});
+		}
 	}
 	
 	public void atStartOfTurn() {
 		p().discardPile.group.stream().filter(c -> c.type == CardType.POWER).forEach(this::act);
+	}
+	
+	public void onVictory() {
+		PRE.clear();
+	}
+	
+	public void onRemove() {
+		PRE.clear();
 	}
 
 }
