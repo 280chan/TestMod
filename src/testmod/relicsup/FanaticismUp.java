2818c58bc5bc1d8735a2f22f70a5bdbf17104a9d
280chan <280262810@qq.com>
2022-06-05 21:15:30 -0700

    new upgraded relic

--- src/testmod/relicsup/FanaticismUp.java
+++ src/testmod/relicsup/FanaticismUp.java
@@ -1,16 +1,17 @@
-package testmod.relics;
+package testmod.relicsup;
 
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
-import testmod.relicsup.FanaticismUp;
+import testmod.relics.Fanaticism;
 
-public class Fanaticism extends AbstractTestRelic {
+public class FanaticismUp extends AbstractUpgradedRelic {
 	
-	public Fanaticism() {
+	public FanaticismUp() {
 		super(RelicTier.BOSS, LandingSound.HEAVY);
 	}
 	
@@ -20,8 +21,6 @@ public class Fanaticism extends AbstractTestRelic {
 	
 	public void onEquip() {
 		this.addEnergy();
-		if (this.inCombat())
-			this.atPreBattle();
     }
 	
 	public void onUnequip() {
@@ -30,7 +29,7 @@ public class Fanaticism extends AbstractTestRelic {
 	
 	private void modifyCounterAndUpdate(int newValue) {
 		this.counter = newValue;
-		this.updateDescription(p().chosenClass);
+		this.updateDescription();
 	}
 	
 	public void atPreBattle() {
@@ -45,30 +44,37 @@ public class Fanaticism extends AbstractTestRelic {
 		if ("Wrath".equals(p().stance.ID)) {
 			this.atb(new GainEnergyAction(1));
 			this.flash();
+		} else if ("Calm".equals(p().stance.ID)) {
+			this.atb(new ChangeStanceAction("Divinity"));
 		}
     }
 	
 	public void onPlayerEndTurn() {
-		if (this.isActive && this.relicStream(FanaticismUp.class).count() == 0) {
-			this.atb(new ChangeStanceAction("Wrath".equals(p().stance.ID) ? "Neutral" : "Wrath"));
+		if (this.isActive && !"Calm".equals(p().stance.ID)) {
+			this.atb(new ChangeStanceAction("Wrath".equals(p().stance.ID) ? "Calm" : "Wrath"));
 			this.flash();
 		}
     }
 	
 	public void onLoseHp(int damage) {
-		this.modifyCounterAndUpdate(this.counter + ("Wrath".equals(p().stance.ID) ? 2 * damage : damage));
+		this.modifyCounterAndUpdate(this.counter + ("Wrath".equals(p().stance.ID) ? 3 * damage : damage));
 	}
 	
 	private int sumCounter() {
-		return this.relicStream(Fanaticism.class).mapToInt(r -> r.counter).sum();
+		return this.relicStream(Fanaticism.class).mapToInt(r -> r.counter).sum()
+				+ this.relicStream(FanaticismUp.class).mapToInt(r -> r.counter).sum();
 	}
 
 	public void onAttack(DamageInfo info, int damage, AbstractCreature target) {
-		if (this.isActive && this.relicStream(FanaticismUp.class).count() == 0 && sumCounter() > 0 && damage > 0) {
+		if (this.isActive && sumCounter() > 0 && damage > 0) {
 			int tmp = sumCounter() * Math.min(damage, target.currentHealth) / 100;
 			if (tmp > 0)
 				this.att(new HealAction(p(), p(), tmp));
 		}
+	}
+
+	public float atDamageModify(float damage, AbstractCard c) {
+		return "Neutral".equals(p().stance.ID) ? damage : 2 * damage;
 	}
 
 }
