e2d324a8ab630768074df4099a26aef4780e56d3
280chan <280262810@qq.com>
2022-06-07 21:59:27 -0700

    nerf and improve Power Strike

--- src/testmod/cards/colorless/PowerStrike.java
+++ src/testmod/cards/colorless/PowerStrike.java
@@ -1,14 +1,26 @@
 package testmod.cards.colorless;
 
+import java.util.ArrayList;
+import java.util.function.Function;
+import com.megacrit.cardcrawl.actions.common.InstantKillAction;
 import com.megacrit.cardcrawl.characters.*;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.*;
-
-import testmod.actions.PowerStrikeAction;
+import com.megacrit.cardcrawl.powers.IntangiblePower;
 import testmod.cards.AbstractTestCard;
 
 public class PowerStrike extends AbstractTestCard {
-    private static final int BASE_MGC = 5;
+    private static final int BASE_MGC = 1;
 
+    public void initializeDescription() {
+		if (this.magicNumber > 0) {
+			int mod = magicNumber % 10, whole = 1 + magicNumber / 10;
+			this.rawDescription = exDesc()[0]
+					+ (mod == 0 ? whole : (magicNumber < 10 ? 1 + ". !M! " : whole + "." + mod)) + exDesc()[1];
+		}
+    	super.initializeDescription();
+    }
+    
     public PowerStrike() {
         super(3, CardType.SKILL, CardRarity.RARE, CardTarget.ALL_ENEMY);
         this.exhaust = this.isEthereal = true;
@@ -16,17 +28,48 @@ public class PowerStrike extends AbstractTestCard {
         this.tags.add(CardTags.STRIKE);
     }
 
-    public void use(final AbstractPlayer p, final AbstractMonster m) {
-    	this.addToBot(new PowerStrikeAction(p, this.magicNumber));
+	private void modify(AbstractMonster m) {
+		int tmp = (int) (Math.log(m.currentHealth) / Math.log(1 + Math.max(this.magicNumber, 1) / 10.0));
+		if (tmp < m.currentHealth && tmp >= 0)
+			m.currentHealth = tmp;
+	}
+
+    public void use(final AbstractPlayer p, final AbstractMonster monster) {
+    	this.addTmpActionToBot(() -> {
+    		if (AbstractDungeon.currMapNode == null || AbstractDungeon.getCurrRoom().monsters == null)
+    			return;
+    		if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead())
+    			return;
+    		ArrayList<AbstractMonster> list = new ArrayList<AbstractMonster>();
+    		ArrayList<AbstractMonster> kill = new ArrayList<AbstractMonster>();
+    		for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+    			if (m.isDead || m.isDying || m.isEscaping || m.escaped)
+    				continue;
+    			this.modify(m);
+    			if (m.currentHealth > 0) {
+    				list.add(m);
+    				m.healthBarUpdatedEvent();
+    			} else {
+    				kill.add(m);
+    			}
+    		}
+    		if (list.size() == 0) {
+    			this.addTmpActionToTop(AbstractDungeon.actionManager::clearPostCombatActions);
+    		} else {
+    			list.stream().map(createIntangible(list.size())).forEach(po -> this.att(apply(p, po)));
+    		}
+    		kill.forEach(m -> this.att(new InstantKillAction(m)));
+    		list.clear();
+    		kill.clear();
+    	});
     }
     
-    public void initializeDescription() {
-		if (this.magicNumber > 4) {
-			int mod = magicNumber % 10, whole = 1 + magicNumber / 10;
-			this.rawDescription = exDesc()[0]
-					+ (mod == 0 ? whole : (magicNumber < 10 ? 1 + ". !M! " : whole + "." + mod)) + exDesc()[1];
-		}
-    	super.initializeDescription();
+    private Function<AbstractMonster, IntangiblePower> createIntangible(int amount) {
+    	return m -> {
+    		IntangiblePower p = new IntangiblePower(m, amount);
+			p.atEndOfTurn(false);
+			return p;
+    	};
     }
     
     public boolean canUpgrade() {
