ffbf1ed89028342044c1534537c99d569d3b4f78
280chan <280262810@qq.com>
2022-06-25 09:13:46 -0700

    rename

---
+++ src/testmod/relics/Recursion.java
@@ -0,0 +1,30 @@
+package testmod.relics;
+
+public class Recursion extends AbstractTestRelic {
+	
+	public Recursion() {
+		super(RelicTier.BOSS, LandingSound.MAGICAL);
+	}
+	
+	private int relicAmount() {
+		return (int) this.relicStream(Recursion.class).count();
+	}
+	
+	public void atPreBattle() {
+		this.counter = 0;
+		if (!isActive)
+			return;
+		p().gameHandSize += 5 * relicAmount();
+    }
+	
+	public void onPlayerEndTurn() {
+		counter++;
+		if (counter == 10) {
+			counter = 0;
+		}
+		if (!isActive)
+			return;
+		p().gameHandSize += (counter == 0 ? 9 : -1) * relicAmount();
+    }
+	
+}
