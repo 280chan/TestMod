ec86dd0b942a861a1bb0caae540afbbe562e43a1
280chan <280262810@qq.com>
2022-07-22 23:00:01 -0700

    new upgraded relic

---
+++ src/testmod/relicsup/RecursionUp.java
@@ -0,0 +1,35 @@
+package testmod.relicsup;
+
+public class RecursionUp extends AbstractUpgradedRelic {
+	
+	public RecursionUp() {
+		super(RelicTier.BOSS, LandingSound.MAGICAL);
+	}
+	
+	public void onEquip() {
+		this.updateHandSize(10);
+		if (this.inCombat()) {
+			this.atPreBattle();
+		}
+	}
+	
+	public void onUnequip() {
+		this.updateHandSize(-10);
+		if (this.inCombat()) {
+			p().gameHandSize -= this.counter;
+		}
+	}
+	
+	public void atPreBattle() {
+		p().gameHandSize += this.counter = 10;
+    }
+	
+	public void onPlayerEndTurn() {
+		counter--;
+		if (counter == 0) {
+			counter = 10;
+		}
+		p().gameHandSize += counter == 10 ? 9 : -1;
+    }
+	
+}
