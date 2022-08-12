ec16815ab9a74552e4cd65cd55ecf255cd623579
280chan <280262810@qq.com>
2022-08-12 10:56:33 -0700

    nerf The Father

--- src/testmod/relics/TheFather.java
+++ src/testmod/relics/TheFather.java
@@ -2,66 +2,69 @@ package testmod.relics;
 
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
+import testmod.mymod.TestMod;
 import testmod.powers.TheFatherPower;
+import testmod.powers.TheFatherPower.TheFatherCounter;
+import testmod.relicsup.TheFatherUp;
 
-public class TheFather extends AbstractTestRelic {
+public class TheFather extends AbstractTestRelic implements TheFatherCounter {
 	private static boolean canUpdate = false;
 	private static int numberOfMonsters = 0;
 	
 	public TheFather() {
 		super(RelicTier.RARE, LandingSound.HEAVY);
-	}
-	
-	public void onEquip() {
 		this.counter = 0;
 	}
 	
+	public void onEquip() {
+		TestMod.setActivity(this);
+		if (this.isActive && this.inCombat() && this.relicStream(TheFatherUp.class).count() == 0) {
+			this.atPreBattle();
+			this.atBattleStart();
+		}
+	}
+	
 	public void atPreBattle() {
-		if (this.hasEnemies()) {
-			tryAdd();
+		if (this.hasEnemies() && this.relicStream(TheFatherUp.class).count() == 0) {
+			TheFatherUp.tryAdd();
 		}
     }
 	
 	public void atBattleStart() {
-		if (TheFatherPower.isPrime(counter + 1)) {
+		if (Prime.isPrime(counter + 1)) {
 			this.beginLongPulse();
 		}
 		canUpdate = true;
 	}
-
-	private void tryAdd() {
-		AbstractDungeon.getMonsters().monsters.stream().filter(not(TheFatherPower::hasThis))
-				.forEach(m -> m.powers.add(new TheFatherPower(m)));
-	}
 	
 	public void update() {
 		super.update();
-		if (canUpdate && this.inCombat()) {
+		if (canUpdate && this.inCombat() && this.relicStream(TheFatherUp.class).count() == 0) {
 			if (AbstractDungeon.getMonsters().monsters.size() > numberOfMonsters) {
-				tryAdd();
+				TheFatherUp.tryAdd();
 				numberOfMonsters = AbstractDungeon.getMonsters().monsters.size();
 			}
 		}
 	}
 	
 	public void atTurnStart() {
-		tryAdd();
+		TheFatherUp.tryAdd();
     }
 	
 	public void count() {
 		this.counter++;
-		if (TheFatherPower.isPrime(counter)) {
+		if (Prime.isPrime(counter)) {
 			p().gainGold(1);
 			this.stopPulse();
 			this.flash();
 		}
-		if (TheFatherPower.isPrime(counter + 1)) {
+		if (Prime.isPrime(counter + 1)) {
 			this.beginLongPulse();
 		}
 	}
 	
 	public void onVictory() {
-		if (this.isActive)
+		if (this.isActive && this.relicStream(TheFatherUp.class).count() == 0)
 			TheFatherPower.reset();
 		this.stopPulse();
 		canUpdate = false;
