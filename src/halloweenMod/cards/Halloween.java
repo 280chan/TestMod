e7dd2e5e7583ae7b043ff597cdf865065918e57b
280chan <280262810@qq.com>
2022-08-16 17:01:16 -0700

    improve card preview

--- src/halloweenMod/cards/Halloween.java
+++ src/halloweenMod/cards/Halloween.java
@@ -1,5 +1,8 @@
 package halloweenMod.cards;
 
+import java.util.ArrayList;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreview;
+import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.cards.*;
 import com.megacrit.cardcrawl.characters.*;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -7,37 +10,48 @@ import com.megacrit.cardcrawl.monsters.*;
 import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.actions.common.*;
 import halloweenMod.mymod.HalloweenMod;
 
 public class Halloween extends AbstractCard {
-    public static final String ID = HalloweenMod.MOD_PREFIX + "Halloween";
-    public static final String IMG = "green/skill/hide";
-    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-    private static final String NAME = cardStrings.NAME;
-    private static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
-    private static final String E = "[E] ";
-    private static final int COST = 0;
-    private static final int BASE_MGC = 1;
-    
-    public Halloween() {
-        this(0);
-        this.cardsToPreview = new Halloween(1);
-    }
-    
-    private Halloween(int toPreview) {
-    	super(ID, NAME, IMG, IMG, COST, getDescription(), CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.SELF);
-        this.baseMagicNumber = BASE_MGC;
-        this.magicNumber = this.baseMagicNumber;
-        this.exhaust = true;
-        for (int i = 0; i < toPreview; i++)
-        	this.upgrade();
-    }
-    
-    private static String getDescription() {
+	public static final String ID = HalloweenMod.MOD_PREFIX + "Halloween";
+	public static final String IMG = "green/skill/hide";
+	private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	private static final String NAME = cardStrings.NAME;
+	private static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
+	private static final String E = "[E] ";
+	private static final int COST = 0;
+	private static final int BASE_MGC = 1;
+	private static final ArrayList<Halloween> PREVIEW = new ArrayList<Halloween>();
+	
+	public Halloween() {
+		this(false);
+		MultiCardPreview.add(this, getPreview(1), getPreview(2));
+	}
+	
+	private Halloween(boolean noPreview) {
+		super(ID, NAME, IMG, IMG, COST, getDescription(), CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL,
+				CardTarget.SELF);
+		this.baseMagicNumber = BASE_MGC;
+		this.magicNumber = this.baseMagicNumber;
+		this.exhaust = true;
+	}
+	
+	private Halloween getPreview(int n) {
+		int old = PREVIEW.size();
+		for (int i = old; i < n; i++) {
+			Halloween tmp = new Halloween(true);
+			for (int j = 0; j < i + 1; j++) {
+				tmp.upgrade();
+			}
+			PREVIEW.add(tmp);
+		}
+		return PREVIEW.get(n - 1);
+	}
+	
+	private static String getDescription() {
 		return getDescription(BASE_MGC);
 	}
-    
+	
 	private static String getDescription(int magic) {
 		String temp = EXTENDED_DESCRIPTION[0] + " ";
 		if (magic < 4)
@@ -51,30 +65,31 @@ public class Halloween extends AbstractCard {
 			return temp + EXTENDED_DESCRIPTION[1];
 	}
 
-    public void use(final AbstractPlayer p, final AbstractMonster m) {
-    	this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber));
-    	this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
-    	this.addToBot(new GainEnergyAction(this.magicNumber));
-    	this.addToBot(new DrawCardAction(p, this.magicNumber));
-    }
-    
-    public AbstractCard makeCopy() {
-        return new Halloween();
-    }
+	public void use(final AbstractPlayer p, final AbstractMonster m) {
+		this.addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber));
+		this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
+		this.addToBot(new GainEnergyAction(this.magicNumber));
+		this.addToBot(new DrawCardAction(p, this.magicNumber));
+	}
+	
+	public AbstractCard makeCopy() {
+		return new Halloween();
+	}
 
-    public boolean canUpgrade() {
-    	return true;
-    }
-    
-    public void upgrade() {
-    	this.timesUpgraded += 1;
-    	this.upgraded = true;
-        this.upgradeMagicNumber(1);
-        this.rawDescription = getDescription(this.magicNumber);
-        this.name = (NAME + "+" + this.timesUpgraded);
-        this.initializeDescription();
-        this.initializeTitle();
-        if (this.cardsToPreview != null)
-        	this.cardsToPreview.upgrade();
-    }
+	public boolean canUpgrade() {
+		return true;
+	}
+	
+	public void upgrade() {
+		this.timesUpgraded += 1;
+		this.upgraded = true;
+		this.upgradeMagicNumber(1);
+		this.rawDescription = getDescription(this.magicNumber);
+		this.name = (NAME + "+" + this.timesUpgraded);
+		this.initializeDescription();
+		this.initializeTitle();
+		ArrayList<AbstractCard> l = MultiCardPreview.multiCardPreview.get(this);
+		if (!PREVIEW.contains(this) && l != null && !l.isEmpty())
+			l.add(getPreview(l.remove(0).timesUpgraded + 2));
+	}
 }
--- src/testmod/cards/colorless/RabbitOfFibonacci.java
+++ src/testmod/cards/colorless/RabbitOfFibonacci.java
@@ -1,21 +1,40 @@
 package testmod.cards.colorless;
 
+import java.util.ArrayList;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreview;
 import com.megacrit.cardcrawl.characters.*;
 import com.megacrit.cardcrawl.helpers.GetAllInBattleInstances;
 import com.megacrit.cardcrawl.monsters.*;
-
-import testmod.cards.AbstractTestCard;
-
 import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import testmod.cards.AbstractTestCard;
 
 public class RabbitOfFibonacci extends AbstractTestCard {
     private static final int BASE_DMG = 1;
     private static final int BASE_BLK = 1;
     private static final double ROOT5 = Math.sqrt(5);
     private static final double LOG_PHI = Math.log((ROOT5 + 1) / 2);
+    private static final ArrayList<RabbitOfFibonacci> PREVIEW = new ArrayList<RabbitOfFibonacci>();
     
     public RabbitOfFibonacci() {
+        this(false);
+		MultiCardPreview.add(this, getPreview(1), getPreview(2), getPreview(3));
+    }
+	
+	private RabbitOfFibonacci getPreview(int n) {
+		int old = PREVIEW.size();
+		for (int i = old; i < n; i++) {
+			RabbitOfFibonacci tmp = new RabbitOfFibonacci(true);
+			for (int j = 0; j < i + 1; j++) {
+				tmp.upgrade();
+			}
+			PREVIEW.add(tmp);
+		}
+		return PREVIEW.get(n - 1);
+	}
+    
+    private RabbitOfFibonacci(boolean preview) {
         super(2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL);
         this.baseDamage = BASE_DMG;
         this.baseBlock = BASE_BLK;
@@ -23,11 +42,9 @@ public class RabbitOfFibonacci extends AbstractTestCard {
     }
 
     public void use(final AbstractPlayer p, final AbstractMonster m) {
-		this.addToBot(new GainBlockAction(p, p, this.block));
-		this.addToBot(
-				new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AttackEffect.SLASH_HORIZONTAL));
-		this.addTmpActionToBot(() -> GetAllInBattleInstances.get(this.uuid).stream().map(c -> (RabbitOfFibonacci) c)
-				.forEach(RabbitOfFibonacci::upgrade));
+		this.atb(new GainBlockAction(p, p, this.block));
+		this.atb(new DamageAllEnemiesAction(p, this.multiDamage, damageTypeForTurn, AttackEffect.SLASH_HORIZONTAL));
+		this.addTmpActionToBot(() -> GetAllInBattleInstances.get(this.uuid).forEach(c -> c.upgrade()));
     }
 
     public boolean canUpgrade() {
@@ -49,5 +66,8 @@ public class RabbitOfFibonacci extends AbstractTestCard {
     	this.upgraded = this.upgradedDamage = this.upgradedBlock = true;
         this.initializeTitle();
         this.baseDamage = this.baseBlock = this.overflow() ? 2147450000 : f(this.timesUpgraded + 2);
+		ArrayList<AbstractCard> l = MultiCardPreview.multiCardPreview.get(this);
+		if (!PREVIEW.contains(this) && l != null && !l.isEmpty())
+			l.add(getPreview(l.remove(0).timesUpgraded + 3));
     }
 }
