247f16a2e74962b77b1667dadebbab069134e25b
280chan <280262810@qq.com>
2022-08-16 17:02:07 -0700

    for code structure changing

--- src/testmod/commands/Test.java
+++ src/testmod/commands/Test.java
@@ -1,11 +1,18 @@
 package testmod.commands;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.AbstractRelic.LandingSound;
 
+import basemod.ReflectionHacks;
 import testmod.cards.AbstractTestCard;
 import testmod.cards.colorless.SunMoon;
 import testmod.cards.colorless.WeaknessCounterattack;
@@ -14,24 +21,24 @@ import testmod.mymod.TestMod;
 @SuppressWarnings("unused")
 public class Test extends TestCommand {
 	
-	private void example() {
-		//*
-		// \u000a cmdHelp();
-		//*/
-	}
-	
 	public void execute(String[] tokens, int depth) {
 		if (tokens.length > 1) {
 			cmdHelp();
 			return;
 		}
 		TestMod.info("升级遗物数量: " + TestMod.UP_RELICS.size());
-		/*p().masterDeck.group.clear();
-		TestMod.CARDS.stream().map(c -> c.makeCopy()).forEach(p().masterDeck.group::add);*/
 	}
 
 	private static void cmdHelp() {
 		tooManyTokensError();
 	}
+	
+	private void example() {
+		//*
+		// \u000a cmdHelp();
+		//*/
+	}
+	
+	
 	
 }
