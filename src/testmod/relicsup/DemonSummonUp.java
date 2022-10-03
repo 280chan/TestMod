dd382ae912625cd0a10efe4c1bdaedf662345263
280chan <280262810@qq.com>
2022-10-03 06:38:42 -0700

    balance Demon Summon+

--- src/testmod/relicsup/DemonSummonUp.java
+++ src/testmod/relicsup/DemonSummonUp.java
@@ -1,41 +1,56 @@
 package testmod.relicsup;
 
+import java.util.ArrayList;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnReceivePowerRelic;
 import com.megacrit.cardcrawl.actions.GameActionManager;
-import com.megacrit.cardcrawl.powers.AbstractPower.PowerType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.DexterityPower;
+import testmod.mymod.TestMod;
 import testmod.relics.DemonSummon;
 
-public class DemonSummonUp extends AbstractUpgradedRelic {
+public class DemonSummonUp extends AbstractUpgradedRelic implements OnReceivePowerRelic {
+	private static ArrayList<Class<? extends AbstractPower>> used = new ArrayList<Class<? extends AbstractPower>>();
+
+	@Override
+	public boolean onReceivePower(AbstractPower p, AbstractCreature s) {
+		if (!(p instanceof InvisiblePower) && used.stream().noneMatch(c -> c.isInstance(p))) {
+			used.add(p.getClass());
+			this.att(apply(p(), new DexterityPower(p(), 1)));
+			this.show();
+		}
+		return true;
+	}
+	
+	private void clear() {
+		if (this.isActive) {
+			used.clear();
+		}
+	}
 	
 	public void onEquip() {
-		if (this.inCombat())
+		if (this.inCombat()) {
 			this.counter = GameActionManager.turn;
+			TestMod.setActivity(this);
+			this.clear();
+		}
 	}
 	
 	public void atPreBattle() {
 		this.counter = 0;
+		this.clear();
 	}
 	
 	public void onVictory() {
 		this.counter = -1;
+		this.clear();
 	}
 	
 	public void atTurnStartPostDraw() {
 		this.counter++;
 		this.att(this.apply(this.p(), DemonSummon.demon(p(), this.counter, this.counter)));
-		this.addTmpActionToBot(() -> att(apply(p(), new DexterityPower(p(), cal()))));
-		this.addTmpActionToBot(() -> p().powers.stream().filter(p -> p instanceof DexterityPower)
-				.map(p -> (DexterityPower) p).forEach(this::modify));
 		this.show();
     }
-	
-	private void modify(DexterityPower p) {
-		this.att(apply(p(), new DexterityPower(p(), p.amount / 2 - p.amount)));
-	}
-	
-	private int cal() {
-		return (int) p().powers.stream().filter(p -> p.type == PowerType.BUFF && !(p instanceof InvisiblePower)).count();
-	}
 	
 }
