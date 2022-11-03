274b82c031cd4ce7dbe76e052bb574f817346f7a
280chan <280262810@qq.com>
2022-11-03 15:18:09 -0700

    minor fix

--- src/testmod/cards/colorless/Bloodthirsty.java
+++ src/testmod/cards/colorless/Bloodthirsty.java
@@ -1,14 +1,12 @@
-
 package testmod.cards.colorless;
 
 import testmod.actions.BloodthirstyAction;
-import testmod.cards.AbstractUpdatableCard;
-
+import testmod.cards.AbstractTestCard;
 import com.megacrit.cardcrawl.cards.*;
 import com.megacrit.cardcrawl.characters.*;
 import com.megacrit.cardcrawl.monsters.*;
 
-public class Bloodthirsty extends AbstractUpdatableCard {
+public class Bloodthirsty extends AbstractTestCard {
 	public static final String ID = "Bloodthirsty";
 	private static final int D_MGC = -2;
 	private static final int UPGRADED_D_MGC = -1;
@@ -19,46 +17,31 @@ public class Bloodthirsty extends AbstractUpdatableCard {
 	}
 
 	public void use(final AbstractPlayer p, final AbstractMonster m) {
-		this.preApplyPowers(p, m);
-		super.applyPowers();
 		this.atb(new BloodthirstyAction(m, p, this.magicNumber));
 		this.used = true;
 	}
-
-	@Override
-	public void preApplyPowers(AbstractPlayer p, AbstractMonster m) {
-		if (m == null) {
-			this.misc = 0;
-			this.onMonster = false;
-			return;
-		}
-		this.onMonster = true;
-		
+	
+	public void calculateCardDamage(AbstractMonster m) {
 		Long max = (long) m.maxHealth;
 		Long rate = (long) this.magicNumber;
 		Long amount = max * rate / 100L;
 		if (amount > 2147483647L)
-			amount = 100000000L;
+			amount = 2000000000L;
 		this.misc = amount.intValue();
 		if (this.misc > m.currentHealth)
 			this.misc = m.currentHealth;
-	}
-	
-	public void applyPowers() {
-		super.applyPowers();
-		this.changeDescription(this.getDesc(), false);
+		super.calculateCardDamage(m);
+		this.rawDescription = this.getDesc();
+		this.initializeDescription();
 	}
 
 	private String getDesc() {
-		return exDesc()[0] + ((this.misc > 0 && this.onMonster && this.isHovered()) ? "(" + this.misc + ")" : "")
-				+ exDesc()[1] + exDesc()[(this.upgraded ? 3 : 2)] + exDesc()[4];
+		return exDesc()[0] + ((this.misc > 0) ? "(" + this.misc + ")" : "") + exDesc()[1]
+				+ exDesc()[(this.upgraded ? 3 : 2)] + exDesc()[4];
 	}
 	
 	public void doublesMagicNumber() {
-		int tmp = this.magicNumber;
-		this.upgradeMagicNumber(this.magicNumber);
-		if (this.magicNumber < 0 && tmp > 0)
-			this.upgradeMagicNumber(2000000000 - this.magicNumber);
+		this.upgradeMagicNumber(this.magicNumber > 1000000000 ? 2000000000 - this.magicNumber : this.magicNumber);
 	}
 	
 	public void triggerOnCardPlayed(AbstractCard c) {
@@ -71,23 +54,11 @@ public class Bloodthirsty extends AbstractUpdatableCard {
 		}
 	}
 	
-	public AbstractCard makeCopy() {
-		Bloodthirsty tmp = new Bloodthirsty();
-		TO_UPDATE.add(tmp);
-		return tmp;
-	}
-
-	public AbstractCard makeStatEquivalentCopy() {
-		Bloodthirsty tmp = (Bloodthirsty)super.makeStatEquivalentCopy();
-		tmp.upgradeMagicNumber(this.magicNumber - tmp.magicNumber);
-		return tmp;
-	}
-	
 	public void upgrade() {
 		if (!this.upgraded) {
 			this.upgradeName();
 			this.upgradeMagicNumber(2);
-			this.changeDescription(upgradedDesc(), true);
+			this.upDesc();
 		}
 	}
 
