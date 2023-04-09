210eb927203ad14b867d0f03150a63ffc1bb7a02
280chan <280262810@qq.com>
2023-04-08 20:16:57 -0700

    minor fix

--- src/testmod/potions/SpacePotion.java
+++ src/testmod/potions/SpacePotion.java
@@ -1,6 +1,8 @@
 package testmod.potions;
 
-import com.megacrit.cardcrawl.actions.common.ObtainPotionAction;
+import java.util.function.Consumer;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.shrines.WeMeetAgain;
@@ -10,6 +12,7 @@ import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
 
 import testmod.mymod.TestMod;
 import testmod.relics.Alchemist;
+import testmod.relicsup.AlchemistUp;
 
 public class SpacePotion extends AbstractTestPotion {
 	public static final String POTION_ID = TestMod.makeID("SpacePotion");
@@ -25,18 +28,42 @@ public class SpacePotion extends AbstractTestPotion {
 	public String getDesc() {
 		return DESCRIPTIONS[0] + this.potency + DESCRIPTIONS[1];
 	}
+	
+	private static class RandomPotionAction extends AbstractGameAction {
+		private Consumer<Integer> action;
+		public RandomPotionAction(int amount, Consumer<Integer> action) {
+			this.amount = amount;
+			this.action = action;
+		}
+
+		@Override
+		public void update() {
+			this.isDone = true;
+			this.action.accept(this.amount);
+		}
+	}
 
 	public void use(AbstractCreature target) {
 		int tmp = this.potency;
 		while (tmp > 0) {
 			tmp--;
-			if (p().potionSlots < 10 || this.relicStream(Alchemist.class).count() > 0) {
+			if (p().potionSlots < 10 || this.relicStream(Alchemist.class).count() > 0
+					|| this.relicStream(AlchemistUp.class).count() > 0) {
 				if (!TestMod.addPotionSlotMultiplayer()) {
 					p().potionSlots++;
 					p().potions.add(new PotionSlot(p().potionSlots - 1));
 				}
+			} else if (p().hasRelic("Sozu")) {
+				p().getRelic("Sozu").flash();
+				break;
 			} else {
-				this.atb(new ObtainPotionAction(AbstractDungeon.returnRandomPotion(true)));
+				this.atb(new RandomPotionAction(tmp + 1, t -> {
+					while (t > 0 && p().potions.stream().anyMatch(s -> s instanceof PotionSlot)) {
+						t--;
+						p().obtainPotion(AbstractDungeon.returnRandomPotion(true));
+					}
+				}));
+				break;
 			}
 		}
 	}
