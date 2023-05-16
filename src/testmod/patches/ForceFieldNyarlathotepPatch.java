e8403bd3124b571aa890dfe9230f81a40b95666a
280chan <280262810@qq.com>
2023-05-15 19:10:38 -0700

    minor fix Nyarlathotep+ and compatible with Chimera

--- src/testmod/patches/ForceFieldNyarlathotepPatch.java
+++ src/testmod/patches/ForceFieldNyarlathotepPatch.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.cards.blue.ForceField;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import testmod.relics.Nyarlathotep;
+import testmod.relicsup.NyarlathotepUp;
 import testmod.utils.MiscMethods;
 
 public class ForceFieldNyarlathotepPatch {
@@ -13,10 +14,13 @@ public class ForceFieldNyarlathotepPatch {
 	public static class ForceFieldPatch {
 		@SpirePrefixPatch
 		public static void Prefix(ForceField c) {
+			if (MiscMethods.MISC.p() == null || MiscMethods.MISC.p().relics == null)
+				return;
 			int r = (int) MiscMethods.MISC.relicStream(Nyarlathotep.class).count();
+			int s = 2 * (int) MiscMethods.MISC.relicStream(NyarlathotepUp.class).count();
 			int n = AbstractDungeon.actionManager.cardsPlayedThisCombat.size();
 			if (r > 0)
-				c.updateCost(- r * n);
+				c.updateCost(- (r + s) * n);
 		}
 	}
 }
